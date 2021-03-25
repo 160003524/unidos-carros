b14c97b5bf25800a744434f9c14135a41591b766
diego <diegoandresrojas2000@gmail.com>
2021-03-25 13:27:13 -0500

    helper : agregando lectura de archivo

---
+++ helpers/read-file.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+const readFile = (path) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, "utf-8", (err, data) => {
+      if (err) reject(err);
+      resolve(data);
+    });
+  });
+};
+
+module.exports = { readFile };
